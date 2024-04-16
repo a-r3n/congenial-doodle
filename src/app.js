ef1442fb05028ac3bbb07d09b73e8d295de43877
a-r3n <alec.renehan@outlook.com>
2024-04-16 16:50:31 +1000

    First run of react code

--- src/app.js
+++ src/app.js
@@ -1,23 +1,15 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import './App.css';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import AboutMe from './pages/AboutMe';
-import Portfolio from './pages/Portfolio';
-import Contact from './pages/Contact';
-import Resume from './pages/Resume';
 
 function App() {
   return (
-    <div className="app">
+    <div className="App">
       <Header />
       <main>
-        <Routes>
-          <Route path="/" element={<AboutMe />} />
-          <Route path="/portfolio" element={<Portfolio />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/resume" element={<Resume />} />
-        </Routes>
+        <AboutMe />
       </main>
       <Footer />
     </div>
